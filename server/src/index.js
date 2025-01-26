1da55c270a8de6764e02c8d55e9badc14aa40b7e
andrercastillon <andrercastillon@gmail.com>
2025-01-26 13:55:37 -0600

    (server): testing server ports

--- server/src/index.js
+++ server/src/index.js
@@ -4,19 +4,24 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Routes
-app.get("/", (req, res) => res.send("Fung.ai API is running"));
+app.get("/", (req, res) => res.send("Fung.ai is running"));
 
-// Connect to MongoDB
-mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+// // Connect to MongoDB
+// const connect = async () => {
+//   try {
+//     await mongoose
+//       .connect(process.env.MONGO)
+//       .then(() => console.log("MongoDB connected"));
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
